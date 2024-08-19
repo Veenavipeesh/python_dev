5f271695a7531920e1071af53255455281a3201f
veena <veena.boninfire@gmail.com>
2024-08-19 15:10:06 +0530

    print elements in a given list

---
+++ venv/elements_in_list.py
@@ -0,0 +1,6 @@
+my_list = input("Enter the list elements space seperated: ")
+list_index = 0
+print("Elements in the given list : ")
+while(list_index < len(my_list)):
+    print (my_list[list_index], end='')
+    list_index += 1
